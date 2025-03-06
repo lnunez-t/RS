e02deb7eb5e498881b47131bf1c0afad6ac03be0
lnunez-t <nuneztor@gmail.com>
2025-03-06 14:46:52 +0100

    debut frontend

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { Card, CardContent } from "@/components/ui/card";
+import { RecycleIcon } from "lucide-react";
+import React from "react";
+
+export default function Home() {
+  const heroData = {
+    quote: '"Donner une nouvelle vie, c\'est creer un futur plus durable"',
+  };
+
+  return (
+    <div className="bg-[#faf2ea] flex flex-row justify-center w-full">
+      <div className="bg-[#faf2ea] w-full max-w-[1440px] relative">
+        {/* <NavbarSection /> */}
+
+        <section className="w-full relative">
+          <div className="w-full h-[545px] relative">
+            
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+
