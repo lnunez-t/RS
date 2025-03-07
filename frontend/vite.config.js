3b7744c81723ac1bbad8c616e98caa70f05db6f9
lnunez-t <nuneztor@gmail.com>
2025-03-07 14:38:00 +0100

    home page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     port: 5173,    // Le port à utiliser
     host: '0.0.0.0', // Permet d'exposer l'app à l'extérieur de Docker
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
